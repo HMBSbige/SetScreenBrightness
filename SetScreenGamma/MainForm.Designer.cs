33ac25a67d8628e943161c1c20ad411940fe8a54
HMBSbige <admin@bige0.com>
2018-01-29 21:56:10 +0800

    Change Brightness Init

--- SetScreenGamma/MainForm.Designer.cs
+++ SetScreenGamma/MainForm.Designer.cs
@@ -31,16 +31,24 @@
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // trackBar1
             // 
             this.trackBar1.AutoSize = false;
             this.trackBar1.BackColor = System.Drawing.Color.Lime;
-            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Location = new System.Drawing.Point(6, 20);
             this.trackBar1.Maximum = 255;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(365, 25);
@@ -52,8 +60,7 @@
             // numericUpDown1
             // 
             this.numericUpDown1.BackColor = System.Drawing.Color.Lime;
-            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.numericUpDown1.Location = new System.Drawing.Point(0, 25);
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 61);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(0);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             255,
@@ -73,7 +80,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button1.Location = new System.Drawing.Point(120, 25);
+            this.button1.Location = new System.Drawing.Point(126, 61);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(245, 21);
@@ -82,14 +89,78 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gamma";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.trackBar2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Brightness";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.BackColor = System.Drawing.Color.Lime;
+            this.trackBar2.Location = new System.Drawing.Point(6, 20);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(365, 25);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Value = 100;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Location = new System.Drawing.Point(126, 61);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.Color.Lime;
+            this.numericUpDown2.Location = new System.Drawing.Point(6, 61);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(0);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 47);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.trackBar1);
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(408, 227);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "MainForm";
@@ -98,6 +169,10 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -106,6 +181,11 @@
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
     }
 }
 
--- SetScreenGamma/MainForm.cs
+++ SetScreenGamma/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Management;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using SetScreenGamma.Properties;
@@ -11,6 +12,29 @@ namespace SetScreenGamma
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Icon = Resources.p90;//load form icon.
+            ChangeGamma(128);//Set to default gamma, because i don't know how to get current system gamma.
+            //Get Current Brightness
+            bLevels = GetBrightnessLevels(); //get the level array for this system
+            if (bLevels.Length == 0) //"WmiMonitorBrightness" is not supported by the system
+            {
+                groupBox2.Enabled = false;
+            }
+            else
+            {
+                trackBar2.TickFrequency = bLevels.Length; //adjust the trackbar ticks according the number of possible brightness levels
+                trackBar2.Maximum = bLevels.Length - 1;
+                numericUpDown2.Maximum = bLevels.Length - 1;
+                trackBar2.Update();
+                trackBar2.Refresh();
+                CheckBrightness(out defaultbrightness);
+            }
+        }
+        
+        #region gamma
 
         [DllImport(@"user32.dll")]
         private static extern IntPtr GetDC(IntPtr hWnd);
@@ -68,13 +92,7 @@ namespace SetScreenGamma
         {
             ChangeGamma(trackBar1.Value);
         }
-
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-            Icon = Resources.p90;
-            ChangeGamma(128);
-        }
-
+        
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
             ChangeGamma(Convert.ToInt32(numericUpDown1.Value));
@@ -83,6 +101,150 @@ namespace SetScreenGamma
         private void button1_Click(object sender, EventArgs e)
         {
             ChangeGamma(128);
+        }
+
+        #endregion
+
+        #region Brightness
+
+        private byte[] bLevels; //array of valid level values
+
+        private int defaultbrightness;
+
+        private void CheckBrightness(out int brightness)
+        {
+            int iBrightness = GetBrightness(); //get the actual value of brightness
+            int i = Array.IndexOf(bLevels, (byte)iBrightness);
+            if (i < 0)
+                i = 1;
+            brightness = i;
+            ChangeBrightness(brightness);
+        }
+
+        private void ChangeBrightness(int i)
+        {
+            try
+            {
+                trackBar2.Value = i;
+                numericUpDown2.Value = i;
+                SetBrightness(bLevels[i]);
+            }
+            catch
+            {
+                //
+            }
+        }
+
+        //get the actual percentage of brightness
+        private static int GetBrightness()
+        {
+            //define scope (namespace)
+            var s = new ManagementScope(@"root\WMI");
+
+            //define query
+            var q = new SelectQuery(@"WmiMonitorBrightness");
+
+            //output current brightness
+            var mos = new ManagementObjectSearcher(s, q);
+
+            var moc = mos.Get();
+
+            //store result
+            byte curBrightness = 0;
+            foreach (var managementBaseObject in moc)
+            {
+                var o = (ManagementObject) managementBaseObject;
+                curBrightness = (byte)o.GetPropertyValue(@"CurrentBrightness");
+                break; //only work on the first object
+            }
+
+            moc.Dispose();
+            mos.Dispose();
+
+            return curBrightness;
+        }
+
+        //array of valid brightness values in percent
+        private static byte[] GetBrightnessLevels()
+        {
+            //define scope (namespace)
+            var s = new ManagementScope(@"root\WMI");
+
+            //define query
+            var q = new SelectQuery(@"WmiMonitorBrightness");
+
+            //output current brightness
+            var mos = new ManagementObjectSearcher(s, q);
+            var BrightnessLevels = new byte[0];
+
+            try
+            {
+                var moc = mos.Get();
+
+                //store result
+
+
+                foreach (var managementBaseObject in moc)
+                {
+                    var o = (ManagementObject) managementBaseObject;
+                    BrightnessLevels = (byte[])o.GetPropertyValue(@"Level");
+                    break; //only work on the first object
+                }
+
+                moc.Dispose();
+                mos.Dispose();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"你的系统不支持亮度控制23333",@"提示",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+
+            return BrightnessLevels;
+        }
+
+        private static void SetBrightness(byte targetBrightness)
+        {
+            //define scope (namespace)
+            var s = new ManagementScope(@"root\WMI");
+
+            //define query
+            var q = new SelectQuery(@"WmiMonitorBrightnessMethods");
+
+            //output current brightness
+            var mos = new ManagementObjectSearcher(s, q);
+
+            var moc = mos.Get();
+
+            foreach (var managementBaseObject in moc)
+            {
+                var o = (ManagementObject) managementBaseObject;
+                o.InvokeMethod(@"WmiSetBrightness", new object[]
+                {
+                    uint.MaxValue, targetBrightness
+                }); //note the reversed order - won't work otherwise!
+                break; //only work on the first object
+            }
+
+            moc.Dispose();
+            mos.Dispose();
+        }
+
+        #endregion
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            ChangeBrightness(trackBar2.Value);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeBrightness(Convert.ToInt32(numericUpDown2.Value));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeBrightness(defaultbrightness);
         }
     }
 }
